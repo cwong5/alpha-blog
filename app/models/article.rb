3f92af958563a84a87e07e5fc835365c9d6b79f5
chester <chester@fuelplus.ca>
2018-07-29 17:14:32 -0400

    Add association and article. added debug view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 2, maximum: 60}
   validates :description, presence: true, length: {minimum: 10, maximum: 600}
-  
+  validates :user_id, presence: true
+
   
 end
