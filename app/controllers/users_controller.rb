812f512953929e06664f276fa6e53fb9d280b9de
chester <chester@fuelplus.ca>
2018-07-29 20:08:39 -0400

    Added edit user account info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,34 @@ class UsersController < ApplicationController
     if @user.save
     flash[:success]  = "Welcome to the Alpha Blog, #{@user.username}"  
     redirect_to articles_path
-    
+   
     else
       render 'new'
     
     end
     
-    
   end
+  
+    def edit
+        @user = User.find(params[:id])
+    end
+  
+  def update
+    
+     @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Account edited"
+      redirect_to articles_path
+    else
+      
+      render 'edit'
+      
+    end
+  end
+  
+  
+  
+  
   
   private
   def user_params
