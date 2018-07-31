a47df78b0330eab35697f0930c37c56281351d2f
chester <chester@fuelplus.ca>
2018-07-30 21:09:01 -0400

    Completed Authnetation system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   
+  
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only:[:edit, :update]
+  
+  
+  
+  
+  
   def new
     @user = User.new
   end
@@ -13,7 +21,6 @@ class UsersController < ApplicationController
   
   def show
    
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5) 
     
   end
@@ -36,12 +43,12 @@ class UsersController < ApplicationController
   end
   
     def edit
-        @user = User.find(params[:id])
+
     end
   
   def update
     
-     @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Account edited"
       redirect_to articles_path
@@ -57,9 +64,22 @@ class UsersController < ApplicationController
   
   
   private
+  
   def user_params
    params.require(:user).permit(:username, :email, :password)
   end
   
+  def set_user
+            @user = User.find(params[:id])
+  end
+  
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "Editing your own stuff only"
+      redirect_to root_path
+    
+    end
+  end
   
 end
